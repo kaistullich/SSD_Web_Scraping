db1a8434b3856ca2723fc7cb4be79b8c533e6267
kaistullich <kairstullich@gmail.com>
2017-03-23 20:36:08 -0700

    all information is now appended to lists and returned from func

--- new_egg.py
+++ new_egg.py
@@ -11,37 +11,43 @@ cur.execute('''CREATE TABLE IF NOT EXISTS ssd_products
 
 
 def scraping(bs):
-    product_title = ''
-    product_price = ''
-    product_shipping = ''
-    product_saving = ''
-
     # Containers that contain the items inside (outermost HTML wrapper)
     containers = bs.find_all("div", {"class": "item-container"})
+
+    # Lists where information will be stored
+    titles = []
+    prices = []
+    shipping = []
+    savings = []
 
     # Loop through all containers and find relevant information
     for container in containers:
         # Product description
         product_title = container.a.img['title']
+        titles.append(product_title)
 
         # Product Price
         price = container.find_all('li', {'class': 'price-current'})
         price_strong = price[0].strong.text
         price_sup = price[0].sup.text
         product_price = price_strong + price_sup
+        prices.append(product_price)
 
         # Product shipping information
-        shipping = container.find_all('li', {'class': 'price-ship'})
-        product_shipping = shipping[0].text.strip()
+        ship = container.find_all('li', {'class': 'price-ship'})
+        product_shipping = ship[0].text.strip()
+        shipping.append(product_shipping)
 
         # If product has any special deals currently
         save = soup.find_all('li', {'class': 'price-save'})
         if save[0].strong is None:
             product_saving = 'No savings currently'
+            savings.append(product_saving)
         else:
             product_saving = save[0].strong.text
+            savings.append(product_saving)
 
-    return product_title, product_price, product_shipping, product_saving
+    return titles, prices, shipping, savings
 
 if __name__ == '__main__':
     # New Egg Internal SSD url
@@ -52,7 +58,7 @@ if __name__ == '__main__':
     web_page_html = web_page.text
     # Create BeautifulSoup object
     soup = BeautifulSoup(web_page_html, 'lxml')
-
+    # All products are stored in this object
     products = scraping(soup)
 
     cur.executemany('INSERT INTO ssd_products VALUES (?, ?, ?, ?, ?)', products)
