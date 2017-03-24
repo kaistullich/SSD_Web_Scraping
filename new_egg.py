92874a81d4ea078a54c8c3d9bfb4649bc4c29301
kaistullich <kairstullich@gmail.com>
2017-03-23 20:28:45 -0700

    put code inside of function and created main block

--- new_egg.py
+++ new_egg.py
@@ -9,23 +9,21 @@ cur = conn.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS ssd_products
               (product_id INTEGER PRIMARY KEY, name TEXT, price REAL, shipping TEXT, saving TEXT)''')
 
-# New Egg Internal SSD url
-url = 'https://www.newegg.com/Internal-SSDs/SubCategory/ID-636'
-# Send request to website
-web_page = requests.get(url)
-# Show HTML (text)
-web_page_html = web_page.text
-# Create BeautifulSoup object
-soup = BeautifulSoup(web_page_html, 'lxml')
 
-# Containers that contain the items inside (outermost HTML wrapper)
-containers = soup.find_all("div", {"class": "item-container"})
+def scraping(bs):
+    product_title = ''
+    product_price = ''
+    product_shipping = ''
+    product_saving = ''
 
-# Loop through all containers and find relevant information
-for container in containers:
-    try:
+    # Containers that contain the items inside (outermost HTML wrapper)
+    containers = bs.find_all("div", {"class": "item-container"})
+
+    # Loop through all containers and find relevant information
+    for container in containers:
         # Product description
         product_title = container.a.img['title']
+
         # Product Price
         price = container.find_all('li', {'class': 'price-current'})
         price_strong = price[0].strong.text
@@ -35,6 +33,7 @@ for container in containers:
         # Product shipping information
         shipping = container.find_all('li', {'class': 'price-ship'})
         product_shipping = shipping[0].text.strip()
+
         # If product has any special deals currently
         save = soup.find_all('li', {'class': 'price-save'})
         if save[0].strong is None:
@@ -42,14 +41,20 @@ for container in containers:
         else:
             product_saving = save[0].strong.text
 
-        cur.execute('INSERT INTO ssd_products VALUES (?, ?, ?, ?, ?)', (product_title, product_price, product_shipping,
-                    product_saving))
-        conn.commit()
-        conn.close()
-        print('Brand: ', product_title)
-        print('Price: ', product_price)
-        print('Shipping: ', product_shipping)
-        print('Savings: ', product_saving)
+    return product_title, product_price, product_shipping, product_saving
 
-    except AttributeError:
-        pass
+if __name__ == '__main__':
+    # New Egg Internal SSD url
+    url = 'https://www.newegg.com/Internal-SSDs/SubCategory/ID-636'
+    # Send request to website
+    web_page = requests.get(url)
+    # Show HTML (text)
+    web_page_html = web_page.text
+    # Create BeautifulSoup object
+    soup = BeautifulSoup(web_page_html, 'lxml')
+
+    products = scraping(soup)
+
+    cur.executemany('INSERT INTO ssd_products VALUES (?, ?, ?, ?, ?)', products)
+    conn.commit()
+    conn.close()
